1c80996a4de0372802b8ae798809e9801392bb81
matias <alejandroarobio@gmail.com>
2023-03-11 18:39:38 -0300

    imprimiendo de forma dinamica las categorias en paginas Upcoming_events y Past_events

--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,5 +1,7 @@
 const galleryEvents = document.querySelector('.galleryEvent')
+const contenedorFiltros = document.querySelector(".form-filters");
 
 pastEvents = data.events.filter((event) => data.currentDate > event.date)
 
 drawCards(pastEvents, galleryEvents)
+drawCategories(getCategories(data.events), contenedorFiltros)
--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -1,5 +1,7 @@
 const galleryEvents = document.querySelector('.galleryEvent');
+const contenedorFiltros = document.querySelector(".form-filters");
 
 upcomingEvents = data.events.filter((event) => data.currentDate < event.date)
 
 drawCards(upcomingEvents, galleryEvents);
+drawCategories(getCategories(data.events), contenedorFiltros)
