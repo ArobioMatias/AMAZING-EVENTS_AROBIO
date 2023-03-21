ebf7b7df864f0299b57a25b09e3f312d137f7d55
matias <alejandroarobio@gmail.com>
2023-03-21 12:05:52 -0300

    completando tablas de estadisticas

---
+++ scripts/stats.js
@@ -0,0 +1,121 @@
+const tableEventsStatistic = document.getElementById('eventsStatistics')
+const tableUpcomingEventsStatistics = document.getElementById('upcomingEventsStatistics')
+const tablePastEventStatistic = document.getElementById('pastEventStatistic')
+
+drawTableStatistic()
+
+async function drawTableStatistic() {
+    const response = await getDataFromAPI()
+    let row = document.createElement('tr')
+    let cellMostAttendance = document.createElement('td')
+    let cellLessAttendance =  document.createElement('td')
+    let cellLargerCapacity = document.createElement('td')
+
+    cellMostAttendance.innerText = `${getMostAttendedEvent(response.events).name} (${getMostAttendedEvent(response.events).percentageAttendace}%)`
+    cellLessAttendance.innerText = `${getlessAttendedEvent(response.events).name} (${getlessAttendedEvent(response.events).percentageAttendace}%)`
+    cellLargerCapacity.innerHTML = `${getLargestCapacityEvent(response.events)[0].name} (${getLargestCapacityEvent(response.events)[0].capacity})`
+
+    row.appendChild(cellMostAttendance)
+    row.appendChild(cellLessAttendance)
+    row.appendChild(cellLargerCapacity)
+    tableEventsStatistic.appendChild(row)
+    /////////////////////////////////////////////////////////////////
+    
+}
+
+function getMostAttendedEvent(events){
+    return getMaxAndMin(events)[0]
+}
+
+function getlessAttendedEvent(events){
+    return getMaxAndMin(events)[1]
+}
+
+function getLargestCapacityEvent(events){
+    return events.sort((a,b)=>{
+        if(a.capacity > b.capacity){
+            return -1
+        }
+        if(a.capacity < b.capacity){
+            return 1
+        }
+        return 0
+    })
+}
+
+function getMaxAndMin(array){
+    const maxAndMin = []
+    const reduceEvents = array.map(event => {
+        let reducedEvent = {}
+        reducedEvent.name = event.name 
+        reducedEvent.percentageAttendace = Number(percentage(event.assistance || event.estimate, event.capacity))
+        return reducedEvent
+    }); 
+
+    reduceEvents.sort((a,b)=>{
+        if(a.percentageAttendace > b.percentageAttendace){
+            return -1
+        }
+        if(a.percentageAttendace < b.percentageAttendace){
+            return 1
+        }
+        return 0
+    })
+    maxAndMin.push(reduceEvents[0])
+    maxAndMin.push(reduceEvents[reduceEvents.length-1])
+    return maxAndMin
+}
+
+function percentage(amount, total){
+    return ((amount*100)/total).toFixed(2)
+}
+
+drawStatisticByCategory()
+
+
+async function drawStatisticByCategory(){
+    const response = await getDataFromAPI()
+    const upcomingEvents = getUpcomingEvents(response)
+    const pastEvents = getPastEvents(response)
+    console.log(pastEvents)
+    const categories = getCategories(response.events)
+    drawStatistic(upcomingEvents, categories, tableUpcomingEventsStatistics)
+    drawStatistic(pastEvents, categories, tablePastEventStatistic)
+}
+
+async function drawStatistic(events, categories, table){
+    categories.forEach(element => {
+        let revenues = 0
+        let percentageAttendance = 0
+        let totalCapacity = 0
+        let totalEstimate = 0
+        let row = document.createElement('tr')
+        let cellCategory = document.createElement('td')
+        let cellRenenues = document.createElement('td')
+        let cellPercentageAttendance = document.createElement('td')
+        let empty = true
+
+        events.forEach(event => {
+            if(element == event.category){
+                empty = false
+                revenues += event.price * (event.estimate || event.assistance)
+                totalCapacity += event.capacity
+                totalEstimate += (event.estimate || event.assistance)
+            }
+        })    
+
+        percentageAttendance = (totalEstimate*100)/totalCapacity
+
+        if(!empty){
+            cellCategory.innerText = element
+            cellRenenues.innerText = `$${revenues}`
+            cellPercentageAttendance.innerText = `${percentageAttendance.toFixed(2)}%`
+    
+            row.appendChild(cellCategory)
+            row.appendChild(cellRenenues)
+            row.appendChild(cellPercentageAttendance)
+            table.appendChild(row)
+        }   
+        
+    })
+}
